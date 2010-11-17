8d8a99e2e5da999379d6e0d0c3bb14862b7f5157
Dougal MacPherson <hello@newfangled.com.au>
2010-11-17 11:19:44 +1100

    Moving the fixtures

--- spec/candle_spec.rb
+++ spec/candle_spec.rb
@@ -3,115 +3,59 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Candle" do
   
   
-  shared_examples_for 'common metadata' do
-    
-    before(:each) do
-      @metadata = @item.metadata
-    end
-    
-    
-    it "should return a hash" do
-      @metadata.should respond_to(:keys)
-    end
+  describe "assets" do
   
-    it "should not be empty" do
-      @metadata.should_not be_empty
-    end
+    shared_examples_for 'common metadata' do
     
-    it "should have a content_type_tree" do
-      @metadata[:content_type_tree].should_not be_empty
-    end
+      before(:each) do
+        @metadata = @item.metadata
+      end
     
     
-  end
+      it "should return a hash" do
+        @metadata.should respond_to(:keys)
+      end
   
-  describe "images" do
-    
-    
-    shared_examples_for 'common bitmap' do
-      
-      it "should be an image" do
-        @item.should be_image
-      end
-      
-      it "should be a bitmap" do
-        @item.should be_bitmap
-      end
-      
-      it "should have width" do
-        @item.metadata[:pixel_width].should_not be_nil
+      it "should not be empty" do
+        @metadata.should_not be_empty
       end
     
-      it "should have height" do
-        @item.metadata[:pixel_height].should_not be_nil
-      end
-      
-    end
-    
-    
-    shared_examples_for 'common composite' do
-
-      it "should be an image" do
-        @item.should be_image
-      end
-      
-      it "should be a composite" do
-        @item.should be_composite
-      end
-      
-    end
-    
-    describe "bitmaps" do
-      
-      %w'gif jpg png psd'.each do |img|
-
-        describe "example.#{img}" do
-      
-          before(:each) do
-    
-            file_path = File.expand_path(File.dirname(__FILE__) + "/fixtures/images/example.#{img}")
-    
-            @file = File.open file_path
-            @item = Candle::Base.new(@file.path)
-          end
-      
+      describe "content type tree" do
         
-          it_should_behave_like 'common metadata'
-          it_should_behave_like 'common bitmap'
-          
-      
+        before(:each) do
+          @content_type_tree = @metadata['kMDItemContentTypeTree']  
+        end
+        
+        it "should have one" do
+          @content_type_tree.should_not be_empty
+        end
+        
+        it "should have at least 3 members" do
+          @content_type_tree.should have_at_least(3).things
         end
       end
+    
+    
     end
     
-    
-    describe "composites" do
-      
-      %w'ai ps pdf'.each do |comp|
-        
-        describe "example.#{comp}" do
-    
-          before(:each) do
   
-            file_path = File.expand_path(File.dirname(__FILE__) + "/fixtures/composites/example.#{comp}")
-  
-            @file = File.open file_path
-            @item = Candle::Base.new(@file.path)
-          end
+    %w'ai eps gif jpg pdf png ps svg'.each do |img|
+
+      describe "example.#{img}" do
     
-          it "should have a creator" do
-            @item.metadata[:creator].should_not be_nil
-          end
-          
-          it_should_behave_like 'common metadata'
-          it_should_behave_like 'common composite'
-          
-        
+        before(:each) do
+  
+          file_path = File.expand_path(File.dirname(__FILE__) + "/fixtures/example.#{img}")
+  
+          @file = File.open file_path
+          @item = Candle::Base.new(@file.path)
         end
-      end
-    end
     
+        it_should_behave_like 'common metadata'
+    
+      end
+      
+    end
   end
-  
 
 end
